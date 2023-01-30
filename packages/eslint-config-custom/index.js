db6c32501fe2b03dda501199a33bffd5d3746650
Thomas JEANNEAU <thomas@essentiel.io>
2023-01-30 20:59:41 +0100

    add eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,15 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next",
+    "turbo",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
   },
 };
