80d0eb13038bd9c6a79005b60c8f07eee7506a81
Thomas JEANNEAU <thomas@essentiel.io>
2023-02-04 18:45:27 +0100

    fix fonts loading

--- apps/bold/src/components/layout.tsx
+++ apps/bold/src/components/layout.tsx
@@ -1,21 +1,17 @@
-import { theme, Components } from '../config'
-import fonts from '../config/fonts'
+import { Components } from '../config'
 
 import type { Component } from 'bold-page'
-import type { Theme } from 'bold-config'
 
 export default function Layout({ components }: { components: Component[] }) {
-  const container = (theme as Theme).container ? 'container' : ''
-  const fontsJoined = fonts ? fonts.join(' ') : ''
   if (components === undefined) throw new Error('No components found in page.')
 
   return (
-    <div className={[container, fontsJoined, 'font-primary'].join(' ')}>
+    <>
       {components.map(({ type, id, props }: Component) => {
         const DynamicComponent = Components[type as keyof typeof Components] as React.FC<Component>
         if (!DynamicComponent) throw new Error(`No component found for type "${type}"`)
         return <DynamicComponent key={id} id={id} type={type} props={props} />
       })}
-    </div>
+    </>
   )
 }
--- apps/bold/src/pages/_app.tsx
+++ apps/bold/src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
+import { theme } from '../config'
+import fonts from '../config/fonts'
+
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import type { Theme } from 'bold-config'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   console.log(metric)
 }
 
 function App({ Component, pageProps }: AppProps) {
+  const container = (theme as Theme).container ? 'container' : ''
+  const fontsJoined = fonts ? fonts.join(' ') : ''
   return (
-    <main>
+    <main className={[container, fontsJoined, 'font-primary'].join(' ')}>
       <Component {...pageProps} />
     </main>
   )
