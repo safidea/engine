cf50d56ef827df3aeb3d193eafcd4114a7ac47fe
Thomas JEANNEAU <thomas@essentiel.io>
2023-02-05 12:08:02 +0100

    add request footer

--- apps/bold/src/config/components/Footer.tsx
+++ apps/bold/src/config/components/Footer.tsx
@@ -1,42 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
+
+import { useTranslation } from 'next-i18next'
 import Link from 'next/link'
 import type { Component } from 'bold-page'
 
+    
 export default function Footer({ props }: Component) {
   const { navigation } = props
-
-  return (
-    <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
-        <nav
-          aria-label="Footer"
-          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-        >
-          {navigation.main.map((item) => (
-            <div key={item.name} className="pb-6">
-              <Link
-                href={item.href}
-                className="text-sm leading-6 text-gray-600 hover:text-gray-900"
-              >
-                {item.name}
-              </Link>
-            </div>
-          ))}
-        </nav>
-        <div className="mt-10 flex justify-center space-x-10">
-          {navigation.social.map((item) => (
-            <Link key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" className="h-6 w-6" />
-            </Link>
-          ))}
-        </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2020 Your Company, Inc. All rights reserved.
-        </p>
-      </div>
-    </footer>
-  )
+  
+  const { t } = useTranslation()
+  
+  
+  return (<footer className="bg-white">
+<div className="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
+<nav aria-label="Footer" className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12">
+{navigation.map((item) => (<div key={item.key} className="pb-6">
+<Link href={item.path} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+{t(`header.navigation.${item.key}`)}
+</Link>
+</div>))}
+</nav><p className="mt-10 text-center text-xs leading-5 text-gray-500">
+{t("footer.copyright")}
+</p>
+</div>
+</footer>)
 }
--- apps/bold/src/config/components/LogoCloudOffWhiteGrid.tsx
+++ apps/bold/src/config/components/LogoCloudOffWhiteGrid.tsx
@@ -1,72 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
+
+import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
 
+
+    
 export default function LogoCloudOffWhiteGrid() {
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
-        <p className="text-center text-lg font-semibold text-gray-600">
-          Trusted by over 5 very average small businesses
-        </p>
-        <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-              alt="Workcation"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-              alt="Mirage"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-              alt="Tuple"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg"
-              alt="Laravel"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-              alt="StaticKit"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-          <div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-            <Image
-              src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg"
-              alt="Statamic"
-              className="max-h-12"
-              width="50"
-              height="50"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  
+  
+  const { t } = useTranslation()
+  
+  
+  return (<div className="bg-white">
+<div className="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
+<p className="text-center text-lg font-semibold text-gray-600">
+{t("customers.title")}
+</p><div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+<div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Workcation" width="138" height="48" />
+</div><div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" width="138" height="48" />
+</div><div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" width="138" height="48" />
+</div><div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel" width="138" height="48" />
+</div><div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" width="138" height="48" />
+</div><div className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+<Image src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg" alt="Statamic" width="138" height="48" />
+</div>
+</div>
+</div>
+</div>)
 }
