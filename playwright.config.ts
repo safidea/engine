e1c96a170b14044680072fb98b9bee19911090b3
Thomas JEANNEAU <thomas@essentiel.io>
2023-06-05 16:01:28 +0200

    update playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { join } from 'path'
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
-  timeout: 60 * 1000,
+  timeout: 3 * 60 * 1000,
   testDir: join(__dirname, 'e2e'),
   retries: 0,
   workers: 4,
