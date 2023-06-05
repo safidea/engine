256775120baf51e9f96d7b1eacd8b856e01adcc8
Thomas JEANNEAU <thomas@essentiel.io>
2023-06-05 15:55:58 +0200

    add console.log to e2e/docker-compose.yml for better clarity

--- e2e/app.ts
+++ e2e/app.ts
@@ -14,12 +14,13 @@ interface AppInterface {
 class App {
   private path: string
   private port: string
+  private name: string
 
   constructor({ config, env, filename }: AppInterface) {
     this.port = env.PORT || '3000'
     env.DATABASE_URL = env.DATABASE_URL ?? `postgresql://admin:admin@db/master`
-    const [name] = filename.match(/[a-z]*(?=\.spec)/) || ['app']
-    this.path = join(__dirname, 'tmp', name)
+    this.name = (filename.match(/[a-z]*(?=\.spec)/) || ['app'])[0]
+    this.path = join(__dirname, 'tmp', this.name)
     fs.ensureDirSync(this.path)
     fs.writeFileSync(join(this.path, 'config.json'), JSON.stringify(config, null, 2))
     fs.writeFileSync(
@@ -30,37 +31,39 @@ class App {
     )
     let compose = fs.readFileSync(join(__dirname, 'docker-compose.yml'), 'utf-8')
     compose = compose.replace(/\$\{APP_PATH\}/g, this.path)
-    compose = compose.replace(/\$\{APP_NAME\}/g, name)
+    compose = compose.replace(/\$\{APP_NAME\}/g, this.name)
     compose = compose.replace('${PORT_APP}', this.port)
     compose = compose.replace('${PORT_DB}', env.DATABASE_URL?.match(/:(\d+)\//)?.[1] || '5432')
     fs.writeFileSync(join(this.path, 'docker-compose.yml'), compose)
   }
 
   public async start(): Promise<void> {
+    const url = 'http://localhost:' + this.port
     execSync(`docker compose -f ${join(this.path, 'docker-compose.yml')} up -d`)
-    return new Promise((resolve, reject) => {
-      const url = 'http://localhost:' + this.port
+    await new Promise<void>((resolve, reject) => {
       let retries = 0
-      const interval = setInterval(async () => {
+      const testUrl = async () => {
+        await new Promise((resolve) => setTimeout(resolve, 3000))
         if (retries++ > 30) {
-          clearInterval(interval)
           reject(new Error(`${url} is not available`))
         }
         http
           .get(url, (res) => {
             if (res.statusCode === 200) {
-              clearInterval(interval)
-              console.log()
               resolve()
             } else {
-              console.log(retries + 's. - waiting for', url)
+              console.log(retries * 3 + 's. - waiting for', url)
+              testUrl()
             }
           })
           .on('error', () => {
-            console.log(retries + 's. - waiting for', url)
+            console.log(retries * 3 + 's. - waiting for', url)
+            testUrl()
           })
-      }, 2000)
+      }
+      testUrl()
     })
+    console.log(`App ${this.name} started on`, url)
   }
 
   public async stop(): Promise<void> {
