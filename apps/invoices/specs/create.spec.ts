bd097066b1f2ec31d0f431495582e7d803ffacb6
Thomas JEANNEAU <thomas@essentiel.io>
2023-06-28 12:53:54 +0200

    create form in current progress

--- apps/invoices/specs/create.spec.ts
+++ apps/invoices/specs/create.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from './fixtures'
 
 test.describe('A page that create an invoices', () => {
   test('should display a title', async ({ page }) => {
@@ -11,22 +11,21 @@ test.describe('A page that create an invoices', () => {
     expect(await page.textContent('h1')).toContain('Créer une facture')
   })
 
-  test.skip('should fill a form and redirect to home page', async ({ page }) => {
+  test.skip('should fill a form and redirect to home page', async ({ page, faker }) => {
     // WHEN
     // I go to the create page "/create"
     await page.goto('/create')
 
     // AND
     // I fill the form
-    await page.fill('input[name="customer"]', 'ESSENTIAL SERVICE')
-    await page.fill('input[name="address"]', '128 RUE LA BÉOTIE 75008 PARIS')
-    await page.fill('input[name="postal_code"]', '75008')
-    await page.fill('input[name="postal_code"]', '75008')
-
-    await page.fill('input[name="activity"]', 'CTO Low Code - Conseil et Développement')
-    await page.fill('input[name="unit"]', 'heure')
-    await page.fill('input[name="quantity"]', '1')
-    await page.fill('input[name="unit_price"]', '1000')
+    await page.fill('input[name="customer"]', faker.company.name())
+    await page.fill('input[name="address"]', faker.location.streetAddress())
+    await page.fill('input[name="zip_code"]', faker.location.zipCode())
+    await page.fill('input[name="country"]', faker.location.country())
+    await page.fill('input[name="activity"]', faker.commerce.productName())
+    await page.fill('input[name="unit"]', faker.commerce.product())
+    await page.fill('input[name="quantity"]', faker.number.int(20))
+    await page.fill('input[name="unit_price"]', faker.number.int({ max: 500 }))
 
     // AND
     // I click on the submit button
