5a9e0e9b0e9e79222a7d4dc8cba4513985c17a39
Thomas JEANNEAU <thomas@essentiel.io>
2023-09-19 17:39:21 +0200

    add green test CreateFileAction

--- src/entities/app/automation/action/storage/createFile/CreateFileAction.test.ts
+++ src/entities/app/automation/action/storage/createFile/CreateFileAction.test.ts
@@ -1,48 +1,58 @@
+import { TableList } from '@entities/app/table/TableList'
 import { describe, test, expect } from '@jest/globals'
-import { TableMapper } from '@adapters/api/table/mappers/TableMapper'
-import { ActionMapper } from '@entities/app/automation/ActionMapper'
-import { HandlebarsTemplating } from '@drivers/templater/handlebars/HandlebarsTemplater'
+import { CreateFileAction } from './CreateFileAction'
+import { BucketList } from '@entities/app/bucket/BucketList'
 
 describe('CreateFileAction', () => {
   test('should render a file with a list of object', async () => {
     // GIVEN
-    const tables = TableMapper.toEntities([
+    const tables = new TableList(
+      [
+        {
+          name: 'tableA',
+          fields: [
+            {
+              name: 'name',
+              type: 'single_line_text',
+            },
+            {
+              name: 'items',
+              type: 'multiple_linked_records',
+              table: 'tableB',
+            },
+          ],
+        },
+        {
+          name: 'tableB',
+          fields: [
+            {
+              name: 'name',
+              type: 'single_line_text',
+            },
+            {
+              name: 'number',
+              type: 'number',
+            },
+          ],
+        },
+      ],
       {
-        name: 'tableA',
-        fields: [
-          {
-            name: 'name',
-            type: 'single_line_text',
-          },
-          {
-            name: 'items',
-            type: 'multiple_linked_records',
-            table: 'tableB',
-          },
-        ],
-      },
-      {
-        name: 'tableB',
-        fields: [
-          {
-            name: 'name',
-            type: 'single_line_text',
-          },
-          {
-            name: 'number',
-            type: 'number',
-          },
-        ],
-      },
-    ])
+        database: {} as any,
+      } as any
+    )
+    const buckets = new BucketList([{ name: 'bucketA' }], { storage: {} as any })
     const converter = {
       htmlToPdf: jest.fn().mockReturnValue('pdf'),
     }
     const storage = {
-      write: jest.fn().mockReturnValue('url'),
+      upload: jest.fn().mockReturnValue('url'),
     }
-    const templating = new HandlebarsTemplating()
-    const action = ActionMapper.toEntity(
+    const templater = {
+      compile: jest.fn((value) => ({
+        render: jest.fn(() => value),
+      })),
+    } as any
+    const action = new CreateFileAction(
       {
         name: 'create_file',
         type: 'create_file',
@@ -58,36 +68,33 @@ describe('CreateFileAction', () => {
           'items.$.number': '{{ record_created.data.items.$.number }}',
         },
       },
-      tables,
-      { converter, storage, templating } as any
+      { converter, storage, templater } as any,
+      { tables, buckets } as any
     )
 
     // WHEN
-    action.execute(
-      {
-        record_created: {
-          table: 'tableA',
-          data: {
-            name: 'Record A',
-            items: [
-              {
-                name: 'Record B',
-                number: 1,
-              },
-              {
-                name: 'Record C',
-                number: 2,
-              },
-            ],
-          },
+    action.execute({
+      record_created: {
+        table: 'tableA',
+        data: {
+          name: 'Record A',
+          items: [
+            {
+              name: 'Record B',
+              number: 1,
+            },
+            {
+              name: 'Record C',
+              number: 2,
+            },
+          ],
         },
       },
-      {} as any
-    )
+    })
 
     // THEN
     expect(converter.htmlToPdf).toHaveBeenCalledWith(
-      '<html><body><p>Record A</p><ul><li>Record B - 1</li><li>Record C - 2</li></ul></body></html>'
+      '<html><body><p>{{name}}</p><ul>{{#each items}}<li>{{ this.name }} - {{ this.number }}</li>{{/each}}</ul></body></html>'
     )
   })
 })
