c00c38db45d2728c407e044ad68a8432ad95bd4a
Thomas JEANNEAU <thomas@essentiel.io>
2023-11-06 11:03:25 +0100

    fix(ci): update release process for auto version license update

--- scripts/update-license-version.sh
+++ scripts/update-license-version.sh
@@ -3,23 +3,15 @@
 VERSION=$1
 LICENSE_FILE="LICENSE"
 
-# Detect the platform (Linux or macOS)
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    SED_CMD="sed -i"
-    DATE_CMD="date -d '+4 years' '+%Y-%m-%d'"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # On macOS, sed -i requires an explicit argument for in-place editing
-    # and date uses -v instead of -d
-    SED_CMD="sed -i ''"
-    DATE_CMD="date -v +4y '+%Y-%m-%d'"
-else
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
+# Update the version number
+perl -pi -e "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $VERSION/" "$LICENSE_FILE"
+
+# Determine and update the change date
+CHANGE_DATE=$(date '+%Y-%m-%d' -d '+4 years' 2>/dev/null) || CHANGE_DATE=$(date -v +4y '+%Y-%m-%d' 2>/dev/null)
+
+if [ -z "$CHANGE_DATE" ]; then
+  echo "Failed to calculate the change date."
+  exit 1
 fi
 
-# Update the version number
-$SED_CMD "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $VERSION/" $LICENSE_FILE
-
-# Update the change date
-CHANGE_DATE=$($DATE_CMD)
-$SED_CMD "s/\(Change Date: \).*$/\1$CHANGE_DATE/" $LICENSE_FILE
+perl -pi -e "s/(Change Date: ).*/\${1}$CHANGE_DATE/" "$LICENSE_FILE"
