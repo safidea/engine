07ac6362929750573970e068b97a73fdaeba765e
Thomas JEANNEAU <thomas@essentiel.io>
2023-11-06 10:52:26 +0100

    fix(ci): update release process for auto deployement on npm

--- scripts/update-license-version.sh
+++ scripts/update-license-version.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Use '' with -i to edit the file in place without a backup on macOS.
-sed -i '' "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $1/" LICENSE
+VERSION=$1
+LICENSE_FILE="LICENSE"
 
-# On macOS, use '-v' to adjust the date.
-sed -i '' "s/\(Change Date: \).*$/\1$(date -v +4y "+%Y-%m-%d")/" LICENSE
+# Detect the platform (Linux or macOS)
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    SED_CMD="sed -i"
+    DATE_CMD="date -d '+4 years' '+%Y-%m-%d'"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # On macOS, sed -i requires an explicit argument for in-place editing
+    # and date uses -v instead of -d
+    SED_CMD="sed -i ''"
+    DATE_CMD="date -v +4y '+%Y-%m-%d'"
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
+
+# Update the version number
+$SED_CMD "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $VERSION/" $LICENSE_FILE
+
+# Update the change date
+CHANGE_DATE=$($DATE_CMD)
+$SED_CMD "s/\(Change Date: \).*$/\1$CHANGE_DATE/" $LICENSE_FILE
