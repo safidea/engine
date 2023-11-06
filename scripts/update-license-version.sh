9270b14e1d4af425ff15836c5d2547d4e60574ab
Thomas JEANNEAU <thomas@essentiel.io>
2023-11-06 10:47:08 +0100

    fix(ci): update release process for auto deployement on npm

--- scripts/update-license-version.sh
+++ scripts/update-license-version.sh
@@ -1,2 +1,7 @@
-sed -i "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $1/" LICENSE
-sed -i "s/\(Change Date: \).*$/\1$(date -d "+4 years" "+%Y-%m-%d")/" LICENSE
+#!/bin/bash
+
+# Use '' with -i to edit the file in place without a backup on macOS.
+sed -i '' "s/Solumy Engine [0-9]*\.[0-9]*\.[0-9]*/Solumy Engine $1/" LICENSE
+
+# On macOS, use '-v' to adjust the date.
+sed -i '' "s/\(Change Date: \).*$/\1$(date -v +4y "+%Y-%m-%d")/" LICENSE
