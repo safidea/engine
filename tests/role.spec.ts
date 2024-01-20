a8436cca273c38e933244c7e208d69071a9837a3
Thomas JEANNEAU <thomas@essentiel.io>
2024-01-20 13:07:31 +0100

    add green test: name should be required

--- tests/role.spec.ts
+++ tests/role.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { Role } from '@solumy/engine/role'
+import { Role, RoleNameRequiredError } from '@solumy/engine/role'
 
 test.describe('Role class', () => {
   test('empty config should return 1 errors', async () => {
@@ -11,5 +11,18 @@ test.describe('Role class', () => {
 
     // THEN
     expect(app.errors).toHaveLength(1)
+  })
+
+  test('name should be required', async () => {
+    // GIVEN
+    const config = {}
+
+    // WHEN
+    const app = new Role(config)
+
+    // THEN
+    const error = app.errors.find((e) => e.message === 'ROLE_NAME_REQUIRED')
+    expect(error).toBeDefined()
+    expect(error).toBeInstanceOf(RoleNameRequiredError)
   })
 })
