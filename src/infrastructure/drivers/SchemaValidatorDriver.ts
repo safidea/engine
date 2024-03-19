872eb028209bc89ba7f8a4872c584349a51a0367
Thomas JEANNEAU <thomas@essentiel.io>
2024-03-19 11:56:13 +0100

    fix(schema): can load schema from npm package

--- src/infrastructure/drivers/SchemaValidatorDriver.ts
+++ src/infrastructure/drivers/SchemaValidatorDriver.ts
@@ -3,6 +3,8 @@ import fs from 'fs-extra'
 import { join } from 'path'
 import type { Driver } from '@adapter/spi/SchemaValidatorSpi'
 
+const dirname = new URL('.', import.meta.url).pathname
+
 export class SchemaValidatorDriver implements Driver {
   private ajv: Ajv
 
@@ -10,8 +12,19 @@ export class SchemaValidatorDriver implements Driver {
     this.ajv = new Ajv({ allErrors: true, allowUnionTypes: true })
   }
 
+  getSchemaPath = (schema: string) => {
+    let schemaPath = join(dirname + '../schemas/', schema + '.schema.json')
+    if (!fs.existsSync(schemaPath)) {
+      schemaPath = join(process.cwd(), 'schemas/', schema + '.schema.json')
+      if (!fs.existsSync(schemaPath)) {
+        throw new Error(`Schema ${schema} not found in ${schemaPath}`)
+      }
+    }
+    return schemaPath
+  }
+
   validateSchema = (data: unknown, schema: string) => {
-    const schemaPath = join(process.cwd(), 'schemas/', schema + '.schema.json')
+    const schemaPath = this.getSchemaPath(schema)
     const schemaJson = fs.readJSONSync(schemaPath)
     const validate = this.ajv.compile(schemaJson)
     if (validate(data)) return []
