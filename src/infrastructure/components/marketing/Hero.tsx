0fcaf4325641988d8139b0d357246a0f6633809f
Thomas JEANNEAU <thomas@essentiel.io>
2024-06-04 17:31:49 +0200

    replace section tag by Container component in marketing component

--- src/infrastructure/components/marketing/Hero.tsx
+++ src/infrastructure/components/marketing/Hero.tsx
@@ -1,16 +1,14 @@
 import type { Props } from '@domain/engine/page/component'
-import { classNames } from '../utils'
+import { Container } from '../base/Container'
 
 export const Hero = ({ id, className = '', Title, Paragraph, Buttons }: Props['Hero']) => (
-  <section id={id} className={classNames('bg-white dark:bg-gray-900', className)}>
-    <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-      <Title size="xl" className="mb-4" />
-      <Paragraph size="lg" className="mb-8" />
-      <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-        {Buttons.map((Button, index) => (
-          <Button key={index} />
-        ))}
-      </div>
+  <Container id={id} className={className} textAlign="center">
+    <Title size="xl" className="mb-4" />
+    <Paragraph size="lg" className="mb-8" />
+    <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+      {Buttons.map((Button, index) => (
+        <Button key={index} />
+      ))}
     </div>
-  </section>
+  </Container>
 )
