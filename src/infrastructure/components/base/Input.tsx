c2674b41420b6a4d61cced1e9e614124a3067c6d
Thomas JEANNEAU <thomas@essentiel.io>
2024-06-06 15:00:36 +0200

    add green test: should render a textarea input

--- src/infrastructure/components/base/Input.tsx
+++ src/infrastructure/components/base/Input.tsx
@@ -1,6 +1,8 @@
 import type { Props } from '@domain/engine/page/component'
 
 export const Input = ({
+  id,
+  className,
   name,
   label,
   type,
@@ -9,7 +11,7 @@ export const Input = ({
   defaultValue,
 }: Props['Input']) => {
   return (
-    <div>
+    <div className={className}>
       {label ? (
         <label
           htmlFor={name}
@@ -18,15 +20,27 @@ export const Input = ({
           {label}
         </label>
       ) : null}
-      <input
-        id={name}
-        type={type}
-        name={name}
-        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-        placeholder={placeholder}
-        required={required}
-        defaultValue={defaultValue}
-      />
+      {type === 'textarea' ? (
+        <textarea
+          rows={3}
+          name={name}
+          id={id ?? name}
+          placeholder={placeholder}
+          required={required}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          defaultValue={defaultValue}
+        ></textarea>
+      ) : (
+        <input
+          id={id ?? name}
+          type={type}
+          name={name}
+          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+          placeholder={placeholder}
+          required={required}
+          defaultValue={defaultValue}
+        />
+      )}
     </div>
   )
 }
---
+++ tests/page/components/base/input.test.ts
@@ -0,0 +1,94 @@
+import { test, expect } from '@tests/fixtures'
+import Page, { type Page as PageConfig } from '@safidea/engine/page'
+import App, { type App as AppConfig } from '@safidea/engine'
+
+test.describe('Input component', () => {
+  test('should render a textarea input', async ({ page }) => {
+    // GIVEN
+    const config: PageConfig = {
+      name: 'Page',
+      path: '/',
+      body: [
+        {
+          component: 'Container',
+          children: [
+            {
+              component: 'Input',
+              type: 'textarea',
+              name: 'description',
+            },
+          ],
+        },
+      ],
+    }
+
+    // WHEN
+    const pageEngine = new Page()
+    const html = await pageEngine.getHtml(config)
+    await page.setContent(html)
+
+    // THEN
+    const input = page.getByRole('textbox')
+    await expect(input).toBeVisible()
+    const isTextarea = await input.evaluate((node) => node.tagName.toLowerCase() === 'textarea')
+    expect(isTextarea).toBeTruthy()
+  })
+
+  test('should display a input in app page', async ({ page }) => {
+    // GIVEN
+    const config: AppConfig = {
+      name: 'App',
+      features: [
+        {
+          name: 'display input',
+          pages: [
+            {
+              name: 'Page',
+              path: '/',
+              body: [
+                {
+                  component: 'Input',
+                  name: 'name',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }
+    const app = new App()
+    const url = await app.start(config)
+
+    // WHEN
+    await page.goto(url)
+
+    // THEN
+    const input = page.getByRole('textbox')
+    await expect(input).toBeVisible()
+  })
+
+  test('should display the input id', async ({ page }) => {
+    // GIVEN
+    const config: PageConfig = {
+      name: 'Page',
+      path: '/',
+      body: [
+        {
+          component: 'Input',
+          name: 'name',
+          id: 'my-input',
+        },
+      ],
+    }
+
+    // WHEN
+    const pageEngine = new Page()
+    const html = await pageEngine.getHtml(config)
+    await page.setContent(html)
+
+    // THEN
+    const input = page.getByRole('textbox')
+    await expect(input).toHaveAttribute('id')
+    expect(await input.getAttribute('id')).toBe('my-input')
+  })
+})
