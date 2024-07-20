35f38b0f450d44ee26413e567ba73406c358d764
Thomas JEANNEAU <thomas@essentiel.io>
2024-07-20 09:56:56 +0200

    fix(types): export all configs types

--- docs/ROADMAP.md
+++ docs/ROADMAP.md
@@ -2,12 +2,14 @@
 
 ## TODO
 
+- [ ] Implement Preline library
 - [ ] Logger Driver should be able to store logs in database
 - [ ] Add a Reporter Driver for Sentry use
 - [ ] Add testimonials component
 - [ ] Add video component
 - [ ] Add theming customization
 - [ ] Add language detection
+- [ ] Be able to run all tests on PostgresSQL
 
 ### Ressources
 
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,32 @@ import type { Drivers as AllDrivers } from '@adapter/spi/Drivers'
 import type { ReactComponents as AllReactComponents } from '@domain/entities/Component'
 
 export type { App } from '@adapter/api/configs/App'
+export type { Table } from '@adapter/api/configs/Table'
+export type { Page } from '@adapter/api/configs/Page'
+export type { Automation } from '@adapter/api/configs/Automation'
+export type { Head } from '@adapter/api/configs/Head'
+export type { Component } from '@adapter/api/configs/Component'
+export type { Action } from '@adapter/api/configs/Action'
+export type { Trigger } from '@adapter/api/configs/Trigger'
+export type { Field } from '@adapter/api/configs/Field'
+export type { Filter } from '@adapter/api/configs/Filter'
+export type { Event } from '@adapter/api/configs/Event'
+export type { Expect } from '@adapter/api/configs/Expect'
+export type { Test } from '@adapter/api/configs/Test'
+
 export type ReactComponents = Partial<AllReactComponents>
 export type Drivers = Partial<AllDrivers>
 
+interface Options {
+  drivers?: Drivers
+  components?: ReactComponents
+}
+
 export default class extends AppApi {
-  constructor(
-    options: {
-      drivers?: Drivers
-      components?: ReactComponents
-    } = {}
-  ) {
+  constructor(options: Options = {}) {
     super({
-      drivers: { ...drivers, ...options?.drivers },
-      components: { ...components, ...options?.components },
+      drivers: { ...drivers, ...options.drivers },
+      components: { ...components, ...options.components },
     })
   }
 }
