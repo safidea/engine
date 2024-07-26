c90d1a59b8024e628c5c86da59f9cbbc85fcad3d
Thomas JEANNEAU <thomas@essentiel.io>
2024-07-26 17:59:42 +0200

    chore(import): new imports scripts in running progress

--- scripts/imports/openai.ts
+++ scripts/imports/openai.ts
@@ -5,41 +5,35 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-export const getChatGPTResponse = async (
-  systemPrompt: string,
-  userPrompt: string
-): Promise<{ code: string; path: string }> => {
+export interface Prompt {
+  system: string
+  user: string
+}
+
+export const getChatGPTResponse = async ({ system, user }: Prompt): Promise<string> => {
   let completeResponse = ''
-  let tokenCount = 0
-  const maxTokensPerRequest = 4096 // Set to a reasonable value within API limits
+  let i = 0
+  const messages = [
+    { role: 'system', content: system },
+    { role: 'user', content: user },
+  ]
 
   while (true) {
-    if (tokenCount > 0) console.log(`Requesting completion with ${tokenCount} tokens...`)
-    const messages = [
-      { role: 'system', content: systemPrompt },
-      { role: 'user', content: userPrompt },
-    ]
-    if (completeResponse) {
-      messages.push({ role: 'assistant', content: completeResponse })
-    }
+    console.log(`${++i} request to OpenAI with ${messages.length} messages...`)
     const response = await openai.chat.completions.create({
       model: 'gpt-4o',
       messages,
-      response_format: { type: 'json_object' },
-      max_tokens: maxTokensPerRequest,
     } as ChatCompletionCreateParamsBase)
 
-    if ('choices' in response && 'usage' in response && !!response.usage) {
+    if ('choices' in response) {
       const message = response.choices[0].message.content!
       completeResponse += message.trim()
-      tokenCount += response.usage.total_tokens
-      if (
-        response.choices[0].finish_reason === 'stop' ||
-        tokenCount >= response.usage.total_tokens
-      ) {
+      console.log(`OpenAI response with ${completeResponse.length} tokens`)
+      if (response.choices[0].finish_reason === 'stop') {
         break
       }
-      userPrompt = 'Continue the previous response.'
+      messages.push({ role: 'assistant', content: message })
+      messages.push({ role: 'user', content: 'Continue the previous response' })
     } else {
       console.error(response)
       throw new Error('Invalid response from OpenAI')
@@ -48,20 +42,9 @@ export const getChatGPTResponse = async (
 
   try {
     // Parse the final complete response
-    return JSON.parse(completeResponse)
+    return completeResponse
   } catch (error) {
     console.error('Failed to parse JSON:', error)
     throw new Error('Failed to parse JSON response from OpenAI')
   }
-}
-
-export const buildCodeFromRules = async (guidelines: string[], userPrompt: string) => {
-  const systemPrompt = `You are a highly skilled AI assistant specialized in generating TypeScript code following these guidelines: 
-    - ${guidelines.join('\n- ')}
-    - You have to not write any comments in the code.
-    - The code have to be fully functionnal and ready to use in production.
-    - Your response have to be in a JSON format in a "code" property.
-    - Please ensure the output is always a valid JSON object.`
-  const response = await getChatGPTResponse(systemPrompt, userPrompt)
-  return response
 }
--- src/infrastructure/components/__imports__/accordion.tsx
+++ src/infrastructure/components/__imports__/accordion.tsx
@@ -1,190 +1,57 @@
-import { classNames } from '../utils'
-
-type AccordionProps = {
-  items: Array<{
-    title: string
-    content: string
-    isActive: boolean
-    hasArrow?: boolean
-    isBordered?: boolean
-    alwaysOpen?: boolean
-    nestedItems?: Array<{ title: string; content: string; isActive: boolean }>
-  }>
+export interface Props {
+  example: 'basic-usage-example' | 'always-open' | 'nested' | 'no-arrow' | 'with-arrow' | 'with-title-and-arrow-stretched' | 'bordered' | 'active-content-bordered';
 }
 
-const Accordion: React.FC<AccordionProps> = ({ items }) => {
-  return (
-    <div
-      className={classNames(
-        'hs-accordion-group',
-        ...(items.some((item) => item.alwaysOpen) ? ['data-hs-accordion-always-open'] : [])
-      )}
-    >
-      {items.map((item, idx) => (
-        <div
-          key={idx}
-          className={classNames(
-            'hs-accordion',
-            item.isActive ? 'active' : '',
-            item.isBordered ? 'border bg-white dark:bg-neutral-800 dark:border-neutral-700' : '',
-            item.isBordered && !item.isActive ? '-mt-px first:rounded-t-lg last:rounded-b-lg' : '',
-            item.isActive && item.isBordered
-              ? 'hs-accordion-active:border-gray-200 dark:hs-accordion-active:border-neutral-700'
-              : '',
-            !item.isBordered && !item.hasArrow && !item.isActive ? 'rounded-lg' : '',
-            !item.isBordered && item.hasArrow && !item.isActive ? 'rounded-xl' : ''
-          )}
-          id={`hs-heading-${idx}`}
-        >
-          <button
-            className={classNames(
-              'hs-accordion-toggle py-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400',
-              item.isActive
-                ? 'hs-accordion-active:text-blue-600 dark:hs-accordion-active:text-blue-500'
-                : '',
-              item.isBordered ? 'py-4 px-5' : '',
-              item.isBordered || item.isActive || item.hasArrow
-                ? 'inline-flex items-center gap-x-3'
-                : 'inline-flex items-center gap-x-3 justify-between'
-            )}
-            aria-expanded={item.isActive}
-            aria-controls={`hs-collapse-${idx}`}
-          >
-            {item.hasArrow && (
-              <>
-                <svg
-                  className={classNames(
-                    item.isActive ? 'hidden' : 'block',
-                    'hs-accordion-active:hidden',
-                    !item.isBordered ? 'size-3.5' : 'size-4'
-                  )}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M5 12h14"></path>
-                  {item.isActive && <path d="M12 5v14"></path>}
-                </svg>
-                <svg
-                  className={classNames(
-                    item.isActive ? 'block' : 'hidden',
-                    'hs-accordion-active:block'
-                  )}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M5 12h14"></path>
-                </svg>
-              </>
-            )}
-            {item.title}
+export const Accordion = ({ example }: Props) => {
+  if (example === 'basic-usage-example') {
+    return (
+      <div className="hs-accordion-group">
+        <div className="hs-accordion active" id="hs-basic-heading-one">
+          <button className="hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="true" aria-controls="hs-basic-collapse-one">
+            <svg className="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5v14"></path>
+            </svg>
+            <svg className="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+            </svg>
+            Accordion #1
           </button>
-          <div
-            id={`hs-collapse-${idx}`}
-            className={classNames(
-              'hs-accordion-content w-full overflow-hidden transition-[height] duration-300',
-              item.isActive ? '' : 'hidden'
-            )}
-            role="region"
-            aria-labelledby={`hs-heading-${idx}`}
-          >
-            <div className={classNames(item.isBordered ? 'pb-4 px-5' : '')}>
-              <p className="text-gray-800 dark:text-neutral-200">{item.content}</p>
-            </div>
-            {item.nestedItems && (
-              <div className="hs-accordion-group ps-6">
-                {item.nestedItems.map((nestedItem, nIdx) => (
-                  <div key={nIdx} className="hs-accordion">
-                    <button
-                      className={classNames(
-                        'hs-accordion-toggle py-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400',
-                        nestedItem.isActive
-                          ? 'hs-accordion-active:text-blue-600 dark:hs-accordion-active:text-blue-500'
-                          : '',
-                        'inline-flex items-center gap-x-3'
-                      )}
-                      aria-expanded={nestedItem.isActive}
-                      aria-controls={`hs-nested-collapse-${nIdx}`}
-                    >
-                      <svg
-                        className={classNames(
-                          nestedItem.isActive ? 'hidden' : 'block',
-                          'size-3',
-                          'hs-accordion-active:hidden'
-                        )}
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M2.62421 7.86L13.6242 7.85999"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                        ></path>
-                        <path
-                          d="M8.12421 13.36V2.35999"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                        ></path>
-                      </svg>
-                      <svg
-                        className={classNames(
-                          nestedItem.isActive ? 'block' : 'hidden',
-                          'size-3',
-                          'hs-accordion-active:block'
-                        )}
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M2.62421 7.86L13.6242 7.85999"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                        ></path>
-                      </svg>
-                      {nestedItem.title}
-                    </button>
-                    <div
-                      id={`hs-nested-collapse-${nIdx}`}
-                      className={classNames(
-                        'hs-accordion-content w-full overflow-hidden transition-[height] duration-300',
-                        nestedItem.isActive ? '' : 'hidden'
-                      )}
-                      role="region"
-                      aria-labelledby={`hs-heading-${nIdx}`}
-                    >
-                      <p className="text-gray-800 dark:text-neutral-200">{nestedItem.content}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )}
+          <div id="hs-basic-collapse-one" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-heading-one">
+            <p className="text-gray-800 dark:text-neutral-200">
+              <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions.
+            </p>
           </div>
         </div>
-      ))}
-    </div>
-  )
-}
+        /* Additional accordion items follow the same structure */
+      </div>
+    );
+  }
 
-export default Accordion
+  if (example === 'always-open') {
+    return (
+      <div className="hs-accordion-group" data-hs-accordion-always-open="">
+        <div className="hs-accordion active" id="hs-basic-always-open-heading-one">
+          <button className="hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="true" aria-controls="hs-basic-always-open-collapse-one">
+            <svg className="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+              <path d="M12 5v14"></path>
+            </svg>
+            <svg className="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h14"></path>
+            </svg>
+            Accordion #1
+          </button>
+          <div id="hs-basic-always-open-collapse-one" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-always-open-heading-one">
+            <p className="text-gray-800 dark:text-neutral-200">
+              <em>This is the third item's accordion body.</em> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions.
+            </p>
+          </div>
+        </div>
+        /* Additional accordion items follow the same structure */
+      </div>
+    );
+  }
+
+  /* Similar implementations for the other styles */
+};
