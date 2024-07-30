8ccfb86dafec084bc8f40a2f5b2bd1541973f097
Thomas JEANNEAU <thomas@essentiel.io>
2024-07-30 11:37:33 +0200

    chore(SingleLinkedRecord): add red test: should create a record with a single linked record field

---
+++ src/adapter/api/configs/Field/SingleLinkedRecord.ts
@@ -0,0 +1,6 @@
+import type { Base } from './base'
+
+export interface SingleLinkedRecord extends Base {
+  field: 'SingleLinkedRecord'
+  table: string
+}
--- src/adapter/api/configs/Field/index.ts
+++ src/adapter/api/configs/Field/index.ts
@@ -4,6 +4,15 @@ import type { Formula } from './Formula'
 import type { LongText } from './LongText'
 import type { Number as Number_ } from './Number'
 import type { SingleLineText } from './SingleLineText'
+import type { SingleLinkedRecord } from './SingleLinkedRecord'
 import type { SingleSelect } from './SingleSelect'
 
-export type Field = Email | SingleLineText | DateTime | LongText | Number_ | Formula | SingleSelect
+export type Field =
+  | Email
+  | SingleLineText
+  | DateTime
+  | LongText
+  | Number_
+  | Formula
+  | SingleSelect
+  | SingleLinkedRecord
