b483e825d3b41c79ad48d8b733ecece287f6a9e1
Thomas JEANNEAU <thomas@essentiel.io>
2024-10-04 10:35:33 +0200

    chore(package): update

--- tests/services/monitor.test.ts
+++ tests/services/monitor.test.ts
@@ -7,7 +7,7 @@ test.describe('Monitor', () => {
   test.describe('Sentry driver', () => {
     test.slow()
 
-    test.only('should send an error', async ({ request }) => {
+    test('should send an error', async ({ request }) => {
       // GIVEN
       const id = nanoid()
       const message = `Test error ${id} for Sentry`
