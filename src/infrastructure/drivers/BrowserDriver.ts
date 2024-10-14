34a5f1e1d349cbf62ef9068ff189ea97b5802459
Thomas JEANNEAU <thomas@essentiel.io>
2024-10-14 14:55:06 +0200

    fix(CI): add sandbox configuration for Puppeteer

--- src/infrastructure/drivers/BrowserDriver.ts
+++ src/infrastructure/drivers/BrowserDriver.ts
@@ -4,7 +4,10 @@ import { BrowserPageDriver } from './BrowserPageDriver'
 
 export class BrowserDriver implements Driver {
   launch = async (baseUrl?: string) => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      // TODO: remove "--no-sandbox" when this issue is fixed on Github Actions
+      args: process.env.CI ? ['--no-sandbox', '--disable-setuid-sandbox'] : [],
+    })
     const page = await browser.newPage()
     return new BrowserPageDriver(browser, page, baseUrl)
   }
