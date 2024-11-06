ce8c5c709140b9537f6a258250234cc04fff50fb
Thomas JEANNEAU <thomas@essentiel.io>
2024-11-06 18:27:03 +0100

    chore(Notion): clear for unit test

--- tests/__helpers__/teardown.ts
+++ tests/__helpers__/teardown.ts
@@ -1,8 +1,10 @@
 import fs from 'fs-extra'
 import { join } from 'path'
 import { container } from './setup'
+import { archiveAllPages } from './integrations/notion'
 
 async function globalTeardown() {
+  await archiveAllPages()
   const isUnitTest = process.argv.includes('tests/unit')
   if (isUnitTest) return
   await container.stop()
