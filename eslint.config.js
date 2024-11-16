f60aa7499be7609127f10dc8df02b48d3a3c0585
Thomas JEANNEAU <thomas@essentiel.io>
2024-11-16 01:14:34 +0100

    fix(eslint): update

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config(
       ],
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
     },
   },
   {
